351af535f96436df618bf60e7f86d6932df3fa35
fb_20 <fb_20@ELYASA>
2024-11-24 14:06:19 +0300

    Add project files.

---
+++ Ornek_Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisiler ks = new Kisiler();
+            string ads;
+            Console.Write("İsim Girin:");
+            ads = Console.ReadLine();
+            ks.kisilistesi(ads);
+            Console.Read();
+        }
+    }
+}
